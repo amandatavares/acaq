580214c25f4f43c4c779ee772c882dc4c6409735
amandatavares <amandatvt@hotmail.com>
2016-10-02 17:54:36 -0300

    Remove laravel sass

--- config/database.php
+++ config/database.php
@@ -68,10 +68,10 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', ''),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
