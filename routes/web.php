cf17828b0cf50c0b3359b5a2669bbb8c52cd2d48
Jonas Albuquerque <jonas_albuquerque@outlook.com.br>
2016-10-03 03:27:53 -0300

    Organização do arquivo de rotas

--- routes/web.php
+++ routes/web.php
@@ -22,54 +22,51 @@ Route::get('/login', function () {
 
 Auth::routes();
 
-Route::get('/following', 'FriendshipController@index');
-
-
-Route::get('/followers', 'FriendshipController@followers');
-
-Route::get('followers/{id}', 'FriendshipController@add_follower');
-Route::get('pesquisa/followers/{id}', 'FriendshipController@add_follower_pesq');
-
-
-Route::get('profile/{id}', 'FriendshipController@show_questions');
-
+//ACAQ
 Route::get('/home', 'HomeController@index');
-Route::get('/categories', 'CategoryController@index');
+Route::get('/profile', 'ProfileController@index');
+//Questions
+Route::resource("perguntas","QuestionController");
+Route::get('/perguntas/myquestions', 'QuestionController@myquestions');
 Route::get('/home', 'QuestionController@index');
-
-Route::get('/laravel', 'LaravelController@index');
-
+//Answers
+Route::get('/perguntas/answer/{id}', 'AnswerController@answer_create');
+Route::post('/perguntas/answer/{id}', 'AnswerController@answer_store');
+//Friendships
+Route::get('/following', 'FriendshipController@index');
+Route::get('/followers', 'FriendshipController@followers');
+Route::get('/followers/{id}', 'FriendshipController@add_follower');
+Route::get('/pesquisa/followers/{id}', 'FriendshipController@add_follower_pesq');
+Route::get('/profile/{id}', 'FriendshipController@show_questions');
+Route::get('/add_user/{id}', 'FriendshipController@add_friend');
+//Categories
+Route::get('/categories', 'CategoryController@index');
 Route::get('/categorias', 'CategoryController@index');
 
+//Others
 Route::post('/pesquisa', function(Request $request){
 	$users = App\User::all();
 	$search = $request->search;
 	return view('pesquisa')->with('users',$users)->with('search',$search);
 });
 
-Route::get('/profile', 'ProfileController@index');
-
-//Jonas's routes
-Route::get('add_user/{id}', 'FriendshipController@add_friend');
-
-Route::get('perguntas/myquestions', 'QuestionController@myquestions');
-Route::get('perguntas/answer/{id}', 'AnswerController@answer_create');
-Route::post('perguntas/answer/{id}', 'AnswerController@answer_store');
-Route::resource("perguntas","QuestionController");
-Route::resource("answers","AnswerController");
-Route::resource("comments","CommentController");
-
-// Create dashboard route
-Route::resource('dashboard', 'DashboardController@getIndex');
-
-Route::get('allusers', function(){
-	$users = App\User::all();
-	return view('allusers')->with('users',$users);
-});
 Route::get('perfil', function(){
 	return view('perfil');
 });
+
 // Curtidas
 Route::post('doLike','LikesController@doLike');
 Route::post('questionLikes','LikesController@questionLikes');
 Route::post('user_like','LikesController@user_like');
+
+//Testes Laravel
+Route::get('/laravel', 'LaravelController@index');
+Route::get('allusers', function(){
+	$users = App\User::all();
+	return view('allusers')->with('users',$users);
+});
+
+//Not_used
+Route::resource("answers","AnswerController");
+Route::resource("comments","CommentController");
+Route::resource('dashboard', 'DashboardController@getIndex');
