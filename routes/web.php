340349aa0d8870846059e05c64cd2c8cc383767f
Débora Moura <deboramourasmd@hotmail.com>
2016-10-07 14:22:04 -0300

    Tirando as barras dos diretorios

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
     return view('auth/index');
 });
 
-Route::get('/login', function () {
+Route::get('login', function () {
     return view('auth/login');
 });
 
@@ -25,46 +25,46 @@ Auth::routes();
 //ACAQ
 //Questions
 Route::resource("perguntas","QuestionController");
-Route::get('/perguntas/myquestions', 'QuestionController@myquestions');
-Route::get('/home', 'QuestionController@index');
-Route::get('/perguntas/delete/{id}', 'QuestionController@delete');
+Route::get('perguntas/myquestions', 'QuestionController@myquestions');
+Route::get('home', 'QuestionController@index');
+Route::get('perguntas/delete/{id}', 'QuestionController@delete');
 
 //Answers
-Route::get('/perguntas/answer/{id}', 'AnswerController@answer_create');
-Route::post('/perguntas/answer/{id}', 'AnswerController@answer_store');
+Route::get('perguntas/answer/{id}', 'AnswerController@answer_create');
+Route::post('perguntas/answer/{id}', 'AnswerController@answer_store');
 Route::get('perguntas/{id}/answer/{id2}/remove', 'AnswerController@destroy');
 Route::get('perguntas/answer/{id}/remove', 'AnswerController@destroy_home');
 
 //Friendships
-Route::get('/following', 'FriendshipController@index');
-Route::get('/following/{id}/remove', 'FriendshipController@unfollow');
-Route::get('/followers', 'FriendshipController@followers');
-Route::get('/followers/{id}', 'FriendshipController@add_follower');
-Route::get('/following/questions', 'FriendshipController@followingQuestions');
-Route::get('/pesquisa/followers/{id}', 'FriendshipController@add_follower_pesq');
-Route::get('/add_user/{id}', 'FriendshipController@add_friend');
+Route::get('following', 'FriendshipController@index');
+Route::get('following/{id}/remove', 'FriendshipController@unfollow');
+Route::get('followers', 'FriendshipController@followers');
+Route::get('followers/{id}', 'FriendshipController@add_follower');
+Route::get('following/questions', 'FriendshipController@followingQuestions');
+Route::get('pesquisa/followers/{id}', 'FriendshipController@add_follower_pesq');
+Route::get('add_user/{id}', 'FriendshipController@add_friend');
 
 //Categorie
-Route::get('/categorias', 'CategoryController@index1');
-Route::get('/category/{id}', 'CategoryController@index');
+Route::get('categorias', 'CategoryController@index1');
+Route::get('category/{id}', 'CategoryController@index');
 
 //Profiles
-Route::post('/profileimg', 'ProfileController@setPicProfile');
-Route::get('/profile', 'ProfileController@index');
-Route::get('/profile/{id}', 'FriendshipController@show_questions');
-Route::get('/profile/followers/{id}', 'FriendshipController@add_follower_prof');
-Route::get('/profile_user/{id2}/followers/{id}', 'FriendshipController@add_follower_prof_user');
-Route::get('/profile_user/{id2}/followers/{id}/remove', 'FriendshipController@unfollow_prof_user');
-Route::get('/profile/perguntas/delete/{id}', 'QuestionController@delete_pergunta_profile');
+Route::post('profileimg', 'ProfileController@setPicProfile');
+Route::get('profile', 'ProfileController@index');
+Route::get('profile/{id}', 'FriendshipController@show_questions');
+Route::get('profile/followers/{id}', 'FriendshipController@add_follower_prof');
+Route::get('profile_user/{id2}/followers/{id}', 'FriendshipController@add_follower_prof_user');
+Route::get('profile_user/{id2}/followers/{id}/remove', 'FriendshipController@unfollow_prof_user');
+Route::get('profile/perguntas/delete/{id}', 'QuestionController@delete_pergunta_profile');
 
 //Others
-Route::post('/pesquisa', function(Request $request){
+Route::post('pesquisa', function(Request $request){
 	$users = App\User::all();
 	$search = $request->search;
 	return view('pesquisa')->with('users',$users)->with('search',$search);
 });
-Route::get('/config', 'ProfileController@config_view');
-Route::post('/config', 'ProfileController@config');
+Route::get('config', 'ProfileController@config_view');
+Route::post('config', 'ProfileController@config');
 
 // Curtidas
 Route::post('doLike','LikesController@doLike');
